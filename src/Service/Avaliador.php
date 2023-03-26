9bea0c8baac5488af60cfa090f0ed0379f6ce46e
Lucas Prado <lukep.onbass@gmail.com>
2023-03-26 11:17:42 -0300

    feat: três maiores lances do leilão

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,14 +2,17 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
-class Avaliador {
+class Avaliador
+{
 
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;
 
-    public function avalia(Leilao $leilao): void 
+    public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
@@ -21,15 +24,28 @@ class Avaliador {
             }
         }
 
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
-    public function getMaiorValor(): float 
+    public function getMaiorValor(): float
     {
         return $this->maiorValor;
     }
 
-    public function getMenorValor(): float 
+    public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
