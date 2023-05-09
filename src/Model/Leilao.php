5015464edb7ad0d64634bc23f6ea557e66813c93
Lucas Prado <lukep.onbass@gmail.com>
2023-05-09 20:47:09 -0300

    feat: trabalhando com excessões

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -18,13 +18,13 @@ class Leilao
     public function recebeLance(Lance $lance)
     {
         if ($this->verificaSeUltimoLanceEhDoUsuarioLanceAtual($lance)) {
-            return;
+            throw new \DomainException('Usuário não pode propor 2 lances consecutivos');
         }
 
         $totalLancesUsuario = $this->getTotalLancesDoUsuario($lance->getUsuario());
 
         if ($totalLancesUsuario >= 5) {
-            return;
+            throw new \DomainException('Usuário não pode propor mais de 5 lances por leilão');
         }
 
         $this->lances[] = $lance;
