9f792cc19f802268f5c4fb502b8a3c099999da1f
Lucas Prado <lukep.onbass@gmail.com>
2023-05-09 20:26:10 -0300

    feat: novas funcionalidades não pode dar lance repetido e maximo 5 lances usuario

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -17,6 +17,16 @@ class Leilao
 
     public function recebeLance(Lance $lance)
     {
+        if ($this->verificaSeUltimoLanceEhDoUsuarioLanceAtual($lance)) {
+            return;
+        }
+
+        $totalLancesUsuario = $this->getTotalLancesDoUsuario($lance->getUsuario());
+
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -26,5 +36,28 @@ class Leilao
     public function getLances(): array
     {
         return $this->lances;
+    }
+
+    private function verificaSeUltimoLanceEhDoUsuarioLanceAtual(Lance $lance) : bool {
+        
+        if (!empty($this->lances) && $lance->getUsuario() == $this->lances[array_key_last($this->lances)]->getUsuario()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getTotalLancesDoUsuario(Usuario $usuario) : int {
+        return array_reduce(
+            $this->lances,
+            function(int $somatorioLances, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $somatorioLances + 1;
+                }
+
+                return $somatorioLances;
+            },
+            0
+        );
     }
 }
